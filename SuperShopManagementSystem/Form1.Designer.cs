83e30ac1799ea3d060d9d1b9420c94f31a5fff93
Sheikh Mahmudul Hasan Shium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2021-07-25 20:40:44 +0600

    test

--- SuperShopManagementSystem/Form1.Designer.cs
+++ SuperShopManagementSystem/Form1.Designer.cs
@@ -45,9 +45,10 @@ namespace SuperShopManagementSystem
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(264, 128);
+            this.label1.Location = new System.Drawing.Point(198, 104);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
             // 
             // panel1
@@ -60,28 +61,26 @@ namespace SuperShopManagementSystem
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Location = new System.Drawing.Point(457, 1);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Location = new System.Drawing.Point(343, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(451, 544);
+            this.panel1.Size = new System.Drawing.Size(338, 442);
             this.panel1.TabIndex = 1;
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(-459, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Location = new System.Drawing.Point(-344, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(464, 436);
+            this.panel2.Size = new System.Drawing.Size(348, 354);
             this.panel2.TabIndex = 5;
             // 
             // btnShowInfo
             // 
             this.btnShowInfo.AutoEllipsis = true;
             this.btnShowInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShowInfo.Location = new System.Drawing.Point(150, 315);
-            this.btnShowInfo.Margin = new System.Windows.Forms.Padding(4);
+            this.btnShowInfo.Location = new System.Drawing.Point(112, 256);
             this.btnShowInfo.Name = "btnShowInfo";
-            this.btnShowInfo.Size = new System.Drawing.Size(172, 46);
+            this.btnShowInfo.Size = new System.Drawing.Size(129, 37);
             this.btnShowInfo.TabIndex = 4;
             this.btnShowInfo.Text = "Log In";
             this.btnShowInfo.UseVisualStyleBackColor = true;
@@ -90,11 +89,11 @@ namespace SuperShopManagementSystem
             // txtPass
             // 
             this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPass.Location = new System.Drawing.Point(250, 241);
-            this.txtPass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPass.Location = new System.Drawing.Point(188, 196);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPass.Name = "txtPass";
             this.txtPass.PasswordChar = '*';
-            this.txtPass.Size = new System.Drawing.Size(143, 34);
+            this.txtPass.Size = new System.Drawing.Size(108, 28);
             this.txtPass.TabIndex = 3;
             // 
             // txtboxUserName
@@ -102,19 +101,20 @@ namespace SuperShopManagementSystem
             this.txtboxUserName.BackColor = System.Drawing.SystemColors.Window;
             this.txtboxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtboxUserName.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtboxUserName.Location = new System.Drawing.Point(250, 189);
-            this.txtboxUserName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtboxUserName.Location = new System.Drawing.Point(188, 154);
+            this.txtboxUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtboxUserName.Name = "txtboxUserName";
-            this.txtboxUserName.Size = new System.Drawing.Size(143, 34);
+            this.txtboxUserName.Size = new System.Drawing.Size(108, 28);
             this.txtboxUserName.TabIndex = 3;
             this.txtboxUserName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(69, 241);
+            this.label4.Location = new System.Drawing.Point(52, 196);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(143, 29);
+            this.label4.Size = new System.Drawing.Size(107, 24);
             this.label4.TabIndex = 1;
             this.label4.Text = "Password\r\n\r\n";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -122,32 +122,34 @@ namespace SuperShopManagementSystem
             // label5
             // 
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(32, 54);
+            this.label5.Location = new System.Drawing.Point(24, 44);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(143, 30);
+            this.label5.Size = new System.Drawing.Size(107, 24);
             this.label5.TabIndex = 1;
             this.label5.Click += new System.EventHandler(this.label3_Click);
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(69, 189);
+            this.label3.Location = new System.Drawing.Point(52, 154);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(143, 29);
+            this.label3.Size = new System.Drawing.Size(107, 24);
             this.label3.TabIndex = 1;
             this.label3.Text = "Username";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Turquoise;
-            this.ClientSize = new System.Drawing.Size(908, 542);
+            this.ClientSize = new System.Drawing.Size(681, 440);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;

d00f2cb296ae6269a984cd9691b413e90c76991d
ProMahabub21 <imahbub.1999@gmail.com>
2021-07-30 16:10:19 +0600

    Sales Form generated

--- SuperShopManagementSystem/SalesForm.cs
+++ SuperShopManagementSystem/SalesForm.cs
@@ -16,5 +16,70 @@ namespace SuperShopManagementSystem
         {
             InitializeComponent();
         }
+
+        private void rbCash_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddProd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sales_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbCard_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbbKash_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDiscount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtQty_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbProductName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCustName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPhone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblUserName_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

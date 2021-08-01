d8695f16ee4c916fbab2745f0078da88f583ccbd
ProgMahabub21 <85748757+ProgMahabub21@users.noreply.github.com>
2021-08-01 21:02:34 +0600

    Add files via upload

--- SuperShopManagementSystem/Form1.cs
+++ SuperShopManagementSystem/Form1.cs
@@ -10,29 +10,34 @@ using System.Windows.Forms;
 
 namespace SuperShopManagementSystem
 {
-    public partial class AddProduct : Form
+    public partial class InvoiceSlip : Form
     {
-        public AddProduct()
+        public InvoiceSlip()
         {
             InitializeComponent();
         }
 
-        private void btnAddProduct_Click(object sender, EventArgs e)
+        private void label4_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void btnEdit_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
         {
 
         }
